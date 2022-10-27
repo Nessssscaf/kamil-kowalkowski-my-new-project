b9239365499a367a4920c3f3007792c8ef9c8f13
Kamil Kowalkowski <https://github.com/Nessssscaf/kamil-kowalkowski-kodilla-java.git>
2022-10-27 21:22:36 +0200

    ograniczenie skanowania w Swaggerze

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,9 +8,8 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableSwagger2
+@OpenAPIDefinition
 @Configuration
 public class CoreConfiguration {
 
@@ -18,12 +18,12 @@ public class CoreConfiguration {
         return new RestTemplate();
     }
 
-/*    @Bean
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
-    }*/
+    }
 }
